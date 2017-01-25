0cb58cb5d966578d95d1711987f397d4b7eda8f6
Brandon High <brandon.high@puppet.com>
2017-01-24 17:36:08 -0800

    Initial mockup of CLI commands for managing the boarding passes
    This commit adds an initial set of commands to the CLI for managing
    boarding passes. I'm continuing to iterate on these.

--- lib/stationmaster/cli.rb
+++ lib/stationmaster/cli.rb
@@ -25,8 +25,143 @@ module Stationmaster
       super args, options, config
     end
 
-    desc "-c, [--config]", _("Manage Stationmaster configuration") + %( ("#{configuration.computed_path}").)
-    map %w[-c --config] => :config
+    class_option :verbose,
+      aliases: "-v",
+      desc: _("Display verbose logging information."),
+      type: :boolean
+
+    class_option :file,
+      aliases: "-f",
+      desc: _("Specify which config file contains the boarding information."),
+      type: :string, default: "boarding.hocon"
+
+    desc "n, [new] NAME", _("Create a new boarding ticket")
+    map %w[n new] => :new_ticket
+    method_option :enable,
+                  aliases: "-e",
+                  desc: _("Enable the boarding ticket."),
+                  type: :boolean, default: true
+    method_option :expiration,
+                  aliases: "-x",
+                  desc: _("When the boarding ticket expires."),
+                  type: :string, default: nil
+    method_option :description,
+                  aliases: "-d",
+                  desc: _("Description of what the boarding ticket is for."),
+                  type: :string, default: nil
+    method_option :ticket,
+                  aliases: "-t",
+                  desc: _("Tracking ticket for the boarding ticket."),
+                  type: :string, default: nil
+    method_option :target_version,
+                  aliases: "-v",
+                  desc: _("The target version for the boarding ticket."),
+                  type: :string, default: nil
+    def new_ticket(name)
+      unless File.exist?(options[:file])
+        say _("Creating boarding config file %{file}...") % { file: options[:file] }
+      end
+
+      say _("Creating new boarding ticket for %{name}...") % { name: name }
+
+      if options[:enable]
+        say _("Enabling %{name}...") % { name: name }
+      else
+        say _("Disabling %{name}...") % { name: name }
+      end
+
+      if options[:expiration]
+        say _("Setting expiration to %{expiration_time} on %{name}...") % { expiration_time: options[:expriation], name: name }
+      else
+        say _("Setting expiration to %{expiration_time} on %{name}...") % { expiration_time: "30 days from now", name: name }
+      end
+
+      if options[:description]
+        say _("Setting description to %{description} on %{name}...") % { description: options[:description], name: name }
+      end
+
+      if options[:ticket]
+        say _("Setting tracking ticket number to %{ticket} on %{name}...") % { ticket: options[:ticket], name: name }
+      end
+
+      if options[:target_version]
+        say _("Setting target version to %{version} on %{name}...") % { version: options[:target_version], name: name }
+      end
+    end
+
+    desc "u, [update] NAME", _("Update a boarding ticket")
+    map %w[u update] => :update_ticket
+    method_option :enable,
+                  aliases: "-e",
+                  desc: _("Enable the boarding ticket."),
+                  type: :boolean, default: nil
+    method_option :expiration,
+                  aliases: "-x",
+                  desc: _("When the boarding ticket expires."),
+                  type: :string, default: nil
+    method_option :description,
+                  aliases: "-d",
+                  desc: _("Description of what the boarding ticket is for."),
+                  type: :string, default: nil
+    method_option :ticket,
+                  aliases: "-t",
+                  desc: _("Tracking ticket for the boarding ticket."),
+                  type: :string, default: nil
+    method_option :target_version,
+                  aliases: "-v",
+                  desc: _("The target version for the boarding ticket."),
+                  type: :string, default: nil
+    def update_ticket(name)
+      config = fetch_config(options[:file])
+
+      say _("Updating boarding ticket %{name}...") % { name: name}
+
+      if options[:enable]
+        say _("Enabling %{name}...") % { name: name }
+      else
+        say _("Disabling %{name}...") % { name: name }
+      end
+
+      if options[:expiration]
+        say _("Setting expiration to %{expiration_time} on %{name}...") % { expiration_time: options[:expriation], name: name }
+      else
+        say _("Setting expiration to %{expiration_time} on %{name}...") % { expiration_time: "30 days from now", name: name }
+      end
+
+      if options[:description]
+        say _("Setting description to %{description} on %{name}...") % { description: options[:description], name: name }
+      end
+
+      if options[:ticket]
+        say _("Setting tracking ticket number to %{ticket} on %{name}...") % { ticket: options[:ticket], name: name }
+      end
+
+      if options[:target_version]
+        say _("Setting target version to %{version} on %{name}...") % { version: options[:target_version], name: name }
+      end
+    end
+
+    desc "s, [show] NAME", _("Show usage of a boarding ticket") % { name: name}
+    map %w[s show] => :show_ticket
+    def show_ticket(name)
+      config = fetch_config(options[:file])
+
+      say _("Grepping around for the ticket %{name}...")
+      say _("When %{name} expires...")
+    end
+
+    desc "b, [board] NAME", _("Board a feature")
+    map %w[b board] => :board_feature
+    def board_feature(name)
+      config = fetch_config(options[:file])
+
+      say _("Boarding feature %{name}...")
+      say _("I hope you've removed all references to the %{name}...") % { name: name}
+      say _("Setting %{name} as boarded so it will throw exceptions if called...") % { name: name}
+    end
+
+    desc "c, [config]", _("Manage Stationmaster configuration") + %( ("#{configuration.computed_path}").)
+    map %w[c config] => :config
     method_option :edit,
                   aliases: "-e",
                   desc: _("Edit Stationmaster configuration."),
@@ -50,10 +185,21 @@ module Stationmaster
       say Identity.version_label
     end
 
-    desc "-h, [--help=COMMAND]", _("Show this message or get help for a command.")
-    map %w[-h --help] => :help
+    desc "-h, [--help=COMMAND], [help COMMAND]", _("Show this message or get help for a command.")
+    map %w[-h --help help] => :help
     def help task = nil
       say and super
+    end
+
+    no_commands do
+      def fetch_config(file)
+        if File.exist?(file)
+          say _("Fetching config from %{file}...") % { file: file }
+        else
+          say _("Boarding config file %{file} does not exist.") % { file: file }
+          exit 1
+        end
+      end
     end
   end
 end
